a40f8b658ff81a6fea78d1cff656a116b2bbacce
Prashant <pkpbhardwaj729@gmail.com>
2015-12-23 15:29:11 +0530

    bug fix in insertion

--- r2robotics/public/drone.php
+++ r2robotics/public/drone.php
@@ -1,9 +1,10 @@
 <?php require_once("../includes/db_connection.php");?>
 <?php
-	$drone_id = $_GET['drone_id'];
-	$altitude = $_GET['altitude'];
-	$latitude = $_GET['latitude'];
-	$longitude = $_GET['longitude'];
+	if (isset($_GET['drone_id'])) { $drone_id = $_GET['drone_id'];	} else { $drone_id = ""; }
+	if (isset($_GET['altitude'])) { $altitude = $_GET['altitude'];	} else { $altitude = ""; }
+	if (isset($_GET['latitude'])) { $latitude = $_GET['latitude'];	} else { $latitude = ""; }
+	if (isset($_GET['longitude'])) { $longitude = $_GET['longitude']; } else { $longitude = ""; }	
+	
 	date_default_timezone_set('Asia/Calcutta');
     $location_time = date("Y-m-d\TH:i:s");
 
