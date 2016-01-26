6bc3bfaca842d69335bdc1824ba79039fcf0b87f
prashant <pkpbhardwaj729@gmail.com>
2016-01-27 02:47:07 +0530

    completed codeygen

---
+++ codeygen/index.php
@@ -0,0 +1,113 @@
+<?php
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "KinG82++");
+define("DB_NAME", "codeygen");
+
+// Create connection
+$conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} 
+?>
+<?php
+  if (isset($_POST['submit'])) {
+    $regno = mysqli_real_escape_string($conn, htmlspecialchars($_POST['regno']));
+    $name = mysqli_real_escape_string($conn, htmlspecialchars($_POST['name']));
+    $email = mysqli_real_escape_string($conn, htmlspecialchars($_POST['email']));
+    $phno = mysqli_real_escape_string($conn, htmlspecialchars($_POST['phno']));
+    $check_query = "SELECT * FROM students";
+    $check_result = mysqli_query($conn, $check_query);
+    confirm_query($check_result);
+    while ($check = mysqli_fetch_assoc($check_result)) {
+      if ($check['regno']==$regno) {
+        $view = "You have already registered."
+      } else {
+        $view = "";
+        $query = "INSERT INTO students (regno, name, email, phno)";
+        $query .= " VALUES ('{$regno}', '{$name}', '{$email}', '{$phno}')";
+        mysqli_query($conn, $query);
+      }
+    }    
+  }
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Code Y Gen</title>
+    <link rel='stylesheet prefetch' href='http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css'>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    
+    <div class='login'>
+        <div class='login_title'>
+            <span>Workshop for Web Development in PHP and MySql</span>
+        </div>
+        <div class='login_fields'>
+            <div class='login_fields__user'>
+                
+                <input placeholder='Registration Number' name="regno" required type='text'>
+                <div class='validation'>
+                    <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/tick.png'>
+                </div>
+                </input>
+            </div>
+            <div class='login_fields__name'>
+                
+                <input placeholder='Name' name="name" required type='text'>
+                <div class='validation'>
+                    <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/tick.png'>
+                </div>
+                </input>
+            </div>
+            <div class='login_fields__email'>
+                
+                <input placeholder='Email' name="email" required type='email'>
+                <div class='validation'>
+                    <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/tick.png'>
+                </div>
+                </input>
+            </div>
+            
+            <div class='login_fields__password'>
+                
+                <input placeholder='Phone Number' name="phno" required type='text'>
+                <div class='validation'>
+                    <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/tick.png'>
+                </div>
+            </div>
+            <div class='login_fields__submit'>
+                <input type='submit' name="submit" value='Submit'>
+                
+            </div>
+            <?php echo $view; ?>
+        </div>
+        <div class='success'>
+            <h2>Succefully Registered</h2>
+            <p>Thank You.</p>
+        </div>
+        <div class='disclaimer'>
+            <p>Developed by Prashant Bhardwaj</p>
+        </div>
+    </div>
+    <div class='authent'>
+        <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/puff.svg'>
+        <p>Submitting...</p>
+    </div>
+    
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src='http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
+    <script src="js/index.js"></script>
+</body>
+
+</html>
+<?php
+if (isset ($conn)){
+  mysqli_close($conn);
+}
+?>
--- codeygen/js/main.js
+++ codeygen/js/main.js
@@ -1,78 +1,42 @@
-$('input[type="submit"]').click(function() {
-    $('.login').addClass('test')
-    setTimeout(function() {
-        $('.login').addClass('testtwo')
-    }, 300);
-    setTimeout(function() {
-        $(".authent").show().animate({
-            right: -320
-        }, {
-            easing: 'easeOutQuint',
-            duration: 600,
-            queue: false
-        });
-        $(".authent").animate({
-            opacity: 1
-        }, {
-            duration: 200,
-            queue: false
-        }).addClass('visible');
-    }, 500);
-    setTimeout(function() {
-        $(".authent").show().animate({
-            right: 90
-        }, {
-            easing: 'easeOutQuint',
-            duration: 600,
-            queue: false
-        });
-        $(".authent").animate({
-            opacity: 0
-        }, {
-            duration: 200,
-            queue: false
-        }).addClass('visible');
-        $('.login').removeClass('testtwo')
-    }, 2500);
-    setTimeout(function() {
-        $('.login').removeClass('test')
-        $('.login div').fadeOut(123);
-    }, 2800);
-    setTimeout(function() {
-        $('.success').fadeIn();
-    }, 3200);
-});
-
-$('input[type="text"],input[type="password"]').focus(function() {
-    $(this).prev().animate({
-        'opacity': '1'
-    }, 200)
-});
-$('input[type="text"],input[type="password"]').blur(function() {
-    $(this).prev().animate({
-        'opacity': '.5'
-    }, 200)
-});
-
-$('input[type="text"],input[type="password"]').keyup(function() {
-    if (!$(this).val() == '') {
-        $(this).next().animate({
-            'opacity': '1',
-            'right': '30'
-        }, 200)
-    } else {
-        $(this).next().animate({
-            'opacity': '0',
-            'right': '20'
-        }, 200)
-    }
-});
-
-var open = 0;
-$('.tab').click(function() {
-    $(this).fadeOut(200, function() {
-        $(this).parent().animate({
-            'left': '0'
-        })
-    });
+$(document).ready(function(){
+  tl = new TimelineLite();
+  var form = $(".sub-form"),
+      sub = $(".submit-under"),
+        allsub = $(".allsub"),
+      loader = $(".loader circle"),
+      loader2 = $(".loader2 circle"),
+      loaders = [loader, loader2],
+      circP = $(".circle-paper");
+  
+  TweenMax.set(sub, {opacity:0.7, rotationY:90}); 
+  TweenMax.set($(".submit-under, .loader, .loader2, .circle-paper, p.success-dialog, h2.success"), {visibility:"visible"}); 
+  TweenMax.set(loader, {drawSVG:'100% 100%', rotation:-90});
+  TweenMax.set(circP, {scaleY:0, transformOrigin:"50% 50%"});
+  TweenMax.set([loader2, circP], {opacity:0});
+  TweenMax.set($(".success, .success-dialog"), {opacity:0});
+  
+  $(".submit").on("click", function(e) {
+    e.preventDefault;
+    tl.to(sub, 1, {opacity:1, rotationY:0, ease:Expo.easeOut});
+    tl.add("flip");
+    tl.to($(".submit"), 0.5, {rotationX:90, ease:Circ.easeOut}, "flip-=1.5");
+    tl.to($(".submit"), 0.5, {opacity:0}, "flip-=0.5");
+    tl.to(sub, 0.25, {css:{borderRadius: "50%", backgroundColor:"#d6c7ca"}, ease:Circ.easeOut}, "flip-=0.5");
+    tl.to(sub, 1.2, {scaleX:0.16,  transformOrigin:"50% 50%", ease:Expo.easeOut}, "flip-=0.5");
+  tl.fromTo(loader, 1, {transformOrigin:"50% 50%", drawSVG:"50% 50%"}, {transformOrigin:"50% 50%", drawSVG:"100%"}, "flip+=1");
+  tl.to(sub, 0.8, {rotationX:90, scaleY:0}, "flip+=1.2");
+  tl.to(loader2, 0.1, {opacity:1}, "flip+=1.8");
+  tl.to(loader2, 0.5, {opacity:1, transformOrigin:"50% 50%", scaleX:0, rotation:180}, "flip+=2");
+    tl.to(loader2, 0.5, {opacity:1, transformOrigin:"50% 50%", scaleX:1, rotation:180}, "flip+=2.5");
+   tl.add("success");
+   tl.to($(".circle-quill"), 0.5, {scaleY:0, transformOrigin:"50% 50%", ease:Circ.easeOut}, "success");
+   tl.to(circP, 0.5, {scaleY:1, opacity:1, transformOrigin:"50% 50%", ease:Circ.easeIn}, "success");
+   tl.to($(".circle-quill"), 0.5, {scaleY:0, transformOrigin:"50% 50%", ease:Circ.easeOut}, "success");
+   tl.to($("input"), 0.5, {scaleY:0}, "success");
+   tl.to($(".info"), 0.5, {scaleY:0}, "success");
+   tl.to($(".success"), 0.5, {scaleY:1, opacity: 1}, "success+=1");
+   tl.to($(".success-dialog"), 1, {opacity: 1}, "success+=1");
+   tl.to(loaders, 0.5, {scaleY:0, stroke:"#b1dbd3", transformOrigin:"50% 50%"}, "success");
+   tl.to(form, 0.5, {css:{backgroundColor:"#7aada5"}}, "success");
+  });
 });
