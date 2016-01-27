81c66a9aa2b5cd2442a5458c5114c55630fa1d92
prashant <pkpbhardwaj729@gmail.com>
2016-01-27 20:10:59 +0530

    added in js

--- codeygen/js/refreshform.js
+++ codeygen/js/refreshform.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
     $("#submit").click(function() {
-	    var regno = $("#regno").val();
-	    var name = $("#name").val();
-	    var email = $("#email").val();
-	    var phno = $("#phno").val();
+        var regno = $("#regno").val();
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var phno = $("#phno").val();
 
-	    // Returns successful data submission message when the entered information is stored in database.
-	    $.post("refreshform.php", {
+        // Returns successful data submission message when the entered information is stored in database.
+        $.post("refreshform.php", {
             regno1: regno,
             name1: name,
             email1: email,
             phno1: phno
-	    },
+        }, function(data) {
+            alert(data);
+            $('#form')[0].reset(); // To reset form fields
+        });
     });
 });
